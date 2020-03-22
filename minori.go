470ae33690810a79bdd84bca2298f6511df0398a
Terra Brown <superloach42@gmail.com>
2020-03-22 13:44:25 -0400

    cleaner time

--- minori.go
+++ minori.go
@@ -56,7 +56,7 @@ func (l *Logger) log(lvl int, from, message string) {
 			continue
 		}
 		fmt.Fprintf(l.Out, "%s | \x1b[%dm%s\x1b[0m | \x1b[35m%s\x1b[0m | \x1b[94m%s\x1b[0m | %s\n",
-			time.Now().Format("2006-01-02 15:04:05"),
+			time.Now().Format("2006 01 02 | 15 04 05"),
 			getColorByLevel(lvl), getMessageByLevel(lvl),
 			l.Name, from, msg,
 		)
